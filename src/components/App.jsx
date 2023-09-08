9419b3370a1f00268fb5891f7356f2111312c8b3
thebestfantasy <sergej.fronch@gmail.com>
2023-09-08 18:00:14 +0300

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Searchbar from './Searchbar/Searchbar';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+    <>
+      <Searchbar onSubmit={}/>
+    </>);
+  }
+}
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+
+export default App;
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Searchbar = onSubmit => {
+  return (
+    <header className="searchbar">
+      <form className="form">
+        <button type="submit" className="button">
+          <span className="button-label">Search</span>
+        </button>
+
+        <input
+          className="input"
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
+
+export default Searchbar;
